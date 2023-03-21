789328f257e046320b67465b1aafee4ace8a874b
Duneth-Kaushalya <dunethkaushalya@gmail.com>
2023-03-21 14:18:07 +0530

    Constructor_Practice

---
+++ src/DefaultValue.java
@@ -0,0 +1,11 @@
+public class DefaultValue {
+    int x;
+    boolean y;
+    public static void main(String[] args) {
+        DefaultValue dv = new DefaultValue();
+        System.out.println(dv.x);
+        System.out.println(dv.y);
+
+    }
+}
+// if the variables have not been assigned any values, it will show the default values as outputs.
